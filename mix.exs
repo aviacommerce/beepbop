0061456c6a0de2b4684951d89b10bd181a7aef18
pkrawat1 <pankajrawat19sept@gmail.com>
2022-07-16 19:14:45 +0530

    upgrade ecto

--- mix.exs
+++ mix.exs
@@ -33,7 +33,7 @@ defmodule BeepBop.MixProject do
       {:credo, "~> 0.9.1", only: :dev, runtime: false},
       {:credo_contrib, "~> 0.1.0-rc", only: :dev, runtime: false},
       {:excoveralls, "~> 0.7", only: :test},
-      {:ecto, "~> 3.8.4"},
+      {:ecto, "~> 3.7.0"},
       {:postgrex, "~> 0.14.0", only: :test},
       {:ex_doc, "~> 0.18", only: :dev, runtime: false}
     ]
